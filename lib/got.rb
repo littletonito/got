1f5d11e27d8a1fefb8c1935ff8f86f1ae9ba98f8
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-06-01 21:07:20 +0000

    modified lib

--- lib/got.rb
+++ lib/got.rb
@@ -4,6 +4,10 @@ module Got
   class Error < StandardError; end
   # Your code goes here...
 end
+
+require 'bundler/setup'
+Bundler.require(:default)
+
 require_relative './got/cli.rb'
 require_relative './got/got.rb'
 require_relative './got/api.rb'
