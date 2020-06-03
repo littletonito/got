2c073637fd3a0b7c91dd2d86dd50d7f2a1c56b73
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-06-03 17:28:07 +0000

    modified lib

--- lib/got/cli.rb
+++ lib/got/cli.rb
@@ -1,3 +1,21 @@
 class Cli 
-  puts "cli class loaded"
+
+  def run 
+    welcome
+    Api.get_got
+    main
+  end
+  
+  def welcome
+    puts "welcome to got CLI"
+  end
+  
+  def main
+    print_all
+  end
+  
+  def print_all
+    Got.all.each {|g| puts "#{g.id}.) #{g.name}"}
+
+
 end
