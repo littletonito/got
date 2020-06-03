2749d09129f1a4536b8c33f9c8c53ad0dccf7d4a
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-06-03 18:00:36 +0000

    modified lib

--- lib/got/cli.rb
+++ lib/got/cli.rb
@@ -1,3 +1,6 @@
+require 'pry'
+
+
 class Cli 
 
   def run 
@@ -6,16 +9,40 @@ class Cli
     main
   end
   
-  def welcome
+  def print_welcome
     puts "welcome to got CLI"
   end
   
   def main
     print_all
+    print_selection_prompt
+    id = prompt_selection
   end
   
   def print_all
     Got.all.each {|g| puts "#{g.id}.) #{g.name}"}
-
+  end
+  
+  def print_selection_prompt
+    puts "Select a characther by number for more information"
+  end
+  
+  def prompt_selection
+    gets.chomp
+  end
+  
+  
+  def valid_id?(id)
+    id = id.to_i
+    if id < 1 || id > Got.all.size 
+      print_error
+      main
+    end
+    id
+  end
+  
+  def print_error
+    puts "Invalid selection, try again!"
+  end
 
 end
