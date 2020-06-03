c84792ecc5ef146b6deed909907e67883772eefe
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-06-03 21:11:59 +0000

    modified lib

--- lib/got/api.rb
+++ lib/got/api.rb
@@ -16,7 +16,7 @@ class Api
   end
   
   def self.get_details_by_id(id)
-    Got.find_by_id(id)
+    selected_got = Got.find_by_id(id)
     res = RestClient.get("#{BASE_URL}#{id}")
     data = JSON.parse(res.body)
   end
